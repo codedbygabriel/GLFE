901f7adc3991423147f9747a757105fb56e38f00
João Gabriel <contato.joaogabriel@proton.me>
2025-09-15 19:06:10 -0300

    feat: Created Move/Rename Handler

--- Program.cs
+++ Program.cs
@@ -22,9 +22,9 @@ namespace GLFE
                 {
                     case ConsoleKey.B:
                         DA.InitExplorerList(PreviousPath: true);
-						break;
+                        break;
                     case ConsoleKey.M:
-                        MoveOptionHandler(ref DA);
+                        ChangeDirOptionHandler(ref DA);
                         break;
                     case ConsoleKey.D:
                         DeleteOptionHandler(ref DA);
@@ -32,39 +32,79 @@ namespace GLFE
                     case ConsoleKey.P:
                         DA.PrintExplorer();
                         break;
+                    case ConsoleKey.R:
+                        RenameOrMoveHandler(ref DA);
+                        break;
                 }
-				Cleaner();
+                Cleaner();
                 DA.PrintHeader();
             }
         }
 
-		static void MoveOptionHandler(ref DirectoryRecover DA) {
-			string res = DA.FindPath();
+        static void RenameOrMoveHandler(ref DirectoryRecover DA)
+        {
+            Console.WriteLine("Original Path...");
+            string OriginalPath = DA.FindPath();
 
-			if(!res.Equals("err"))
-			{
-				DA.InitExplorerList(res);
-			}
-		}
+            if (!OriginalPath.Equals("err"))
+            {
+                Console.Write("(R)ename or (M)ove? ");
+                ConsoleKeyInfo Catch = Console.ReadKey(true);
+
+                Console.Write("Where To Path/File...");
+                string WhereToPath = Console.ReadLine() ?? "err";
+
+
+                try
+                {
+                    if (DA.IsPathDir(OriginalPath))
+                    {
+                        Directory.Move(sourceDirName: OriginalPath, destDirName: WhereToPath);
+                    }
+                    else
+                    {
+                        File.Move(sourceFileName: OriginalPath, destFileName: WhereToPath);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something wen't wrong while moving / renaming file.\n" + e);
+                }
+            }
+
+        }
+
+        static void ChangeDirOptionHandler(ref DirectoryRecover DA)
+        {
+            string res = DA.FindPath();
+
+            if (!res.Equals("err"))
+            {
+                DA.InitExplorerList(res);
+            }
+        }
 
         static void DeleteOptionHandler(ref DirectoryRecover DA)
         {
-			string path = ClipboardUtils.GetFromClipboard();
+            string path = ClipboardUtils.GetFromClipboard();
 
-			if (string.IsNullOrEmpty(path)){
-				Console.Write("File / Directory: ");
-				path = Console.ReadLine() ?? "err";
-			}
+            if (string.IsNullOrEmpty(path))
+            {
+                Console.Write("File / Directory: ");
+                path = Console.ReadLine() ?? "err";
+            }
 
             Console.Write($"Are you sure? [{path}]");
 
             if (Console.ReadKey().Key == ConsoleKey.Y)
             {
                 DA.RemoveFileOrDirectory(path);
-				Console.WriteLine();
-            } else {
-				Console.WriteLine("\nOperation Aborted");
-			}
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("\nOperation Aborted");
+            }
         }
 
         static void Cleaner()
